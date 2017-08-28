7e41b4401cc10f0553909c2b09b5c7cb3ce956bc
wp-src <wpmagic70@gmail.com>
2017-08-28 21:31:59 +0430

    -

--- app/Watermark/Commands/WatermarkCommand.php
+++ app/Watermark/Commands/WatermarkCommand.php
@@ -248,24 +248,4 @@ class WatermarkCommand extends UserCommand
 
         return null;
     }
-
-    public function downloadFile($url, $path)
-    {
-        $newfname = $path;
-        $file = fopen($url, 'rb');
-        if ($file) {
-            $newf = fopen($newfname, 'wb');
-            if ($newf) {
-                while (!feof($file)) {
-                    fwrite($newf, fread($file, 1024 * 8), 1024 * 8);
-                }
-            }
-        }
-        if ($file) {
-            fclose($file);
-        }
-        if ($newf) {
-            fclose($newf);
-        }
-    }
 }
